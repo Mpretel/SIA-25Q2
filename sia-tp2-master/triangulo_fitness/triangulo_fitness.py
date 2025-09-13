2f1003d3ca1f69ad7a26dc1a94ad70371cb637b0
FFCriscuolo <facundocriscuolo@gmail.com>
2025-09-13 17:18:45 -0300

    added fitness plots

---
+++ sia-tp2-master/triangulo_fitness/triangulo_fitness.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import glob
+import os
+
+csv_files = glob.glob("triangulo_fitness/*.csv")
+
+plt.figure(figsize=(10, 6))
+
+for file in csv_files:
+    df = pd.read_csv(file)
+    plt.plot(df["generation"], df["best_fitness"], label=os.path.splitext(os.path.basename(file))[0], linewidth=2)
+
+plt.xlabel("Generación", fontsize=14)
+plt.ylabel("Fitness", fontsize=14)
+#plt.title("Evolución del Fitness", fontsize=16)
+plt.legend(fontsize=10, loc='lower right')
+plt.grid(True)
+plt.show()

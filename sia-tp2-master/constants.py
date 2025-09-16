7b63ea1cbd93de895ad1dacfb417e937c411fa3a
Lucila Capurro <95888088+lucilacapurro@users.noreply.github.com>
2025-09-16 11:59:47 -0300

    Comentarios

--- sia-tp2-master/constants.py
+++ sia-tp2-master/constants.py
@@ -1,17 +1,26 @@
-CONFIGS_DIR = "configs"
-INPUT_IMAGES_DIR = "input_images"
-OUTPUT_IMAGES_DIR = "ga_output"
+# Main directories for execution
+CONFIGS_DIR = "configs"            # Folder where algorithm configurations are stored
+INPUT_IMAGES_DIR = "input_images"  # Folder containing the original images to compress
+OUTPUT_IMAGES_DIR = "ga_output"    # Folder where results are saved (generated images, logs, etc.)
 
-SCALE_FACTOR = 3
+# Image scaling factor
+SCALE_FACTOR = 3  # Scale applied to resize input images (reduce teh size)
 
-MIN_ALPHA = 50
-MAX_ALPHA = 255
+# Transparency parameters (alpha channel of the triangles --> min alpha avoids fully transparent triangles)
+MIN_ALPHA = 50    # Minimum transparency (0 = fully transparent, 255 = fully opaque)
+MAX_ALPHA = 255   # Maximum transparency
 
-MIN_AREA = 1000
-MAX_AREA = 10000
+# Constraints on triangle area
+MIN_AREA = 1000   # Minimum allowed area for a triangle
+MAX_AREA = 10000  # Maximum allowed area for a triangle
 
-SEED = 43
+# Randomness seed
+SEED = 43  # Fixes the random generator to make runs reproducible
 
-RGB = True
+# Color representation configuration
+RGB = True   # If True → use RGB colors (red, green, blue)
+             # If False, HSV (hue, saturation, value)
 
-DELTA = False
+# Delta mutation flag
+DELTA = False  # If True → mutations apply incremental changes (deltas)
+               # If False → random values are generated
